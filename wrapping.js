cbdc9b7dafe3e675bc81658a9fd085faf72bf099
Martin Küchler <mkuechle@hs-mittweida.de>
2018-09-19 12:14:40 +0200

    fixed wrapping

--- wrapping.js
+++ wrapping.js
@@ -1,9 +1,9 @@
 var Sloffle = require("sloffle")
+const { exec } = require('child_process');
 
 const main = async () => {
 
     await Sloffle.wrapping("solidity_modules/ew-user-registry-contracts/dist", "dist/ts/wrappedContracts")
-    // await Sloffle.wrapping()
 
 }
 
